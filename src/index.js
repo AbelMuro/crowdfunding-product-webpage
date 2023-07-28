4eb298ee66a1e0eded0f478861adda9386871448
abel muro <abelmuro93@gmail.com>
2023-07-28 14:44:22 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const rootRef = document.querySelector('#root');
+
+const root = ReactDOM.createRoot(rootRef);
+root.render(<>hello world</>);
